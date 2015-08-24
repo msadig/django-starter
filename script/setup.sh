f8de4b9d3ae1ac9a2847d2f7444f7e5448f9fb3b
Sadig Muradov <sadig@ymail.com>
2015-08-25 03:36:41 +0500

    flow logic improved

--- script/setup.sh
+++ script/setup.sh
@@ -9,34 +9,34 @@ rm master.zip
 cd ./django-starter-master
 
 
-echo "Config path: "
-read -e confpath
-
-echo "Auto configuration? (y/n)"
+echo "Create configuration? (y/n)"
 read -e run
 if [ "$run" == n ] ; then
+	echo "Path of the existing config file: "
+	read -e confpath
 	sed -i.bak -e 's|#{CONFIGPATH}|'$confpath'|g' script/app.sh
 	sed -i.bak -e 's|#{CONFIGPATH}|'$confpath'|g' script/destroy.sh
 	rm -rfv script/*.bak
 exit
 else
 	rm -rfv config.txt
-	echo "Site URL or IP: "
+	echo "=> Site URL or IP: "
 	read -e server
-	echo "App's Server User: "
+	echo "=> App's Server User: "
 	read -e appuser
-	echo "App Name: "
+	echo "=> App Name: "
 	read -e appname
-	echo "Path to app: "
+	echo "=> Path to app: "
 	read -e apppath
-	echo "Database User: "
+	echo "=> Database User: "
 	read -e db_user
-	echo "Database User's password: "
+	echo "=> Database User's password: "
 	read -e db_password
-	echo "Database Name: "
+	echo "=> Database Name: "
 	read -e db_name
 
 	sed 's|#{server}|'$server'|g' tpl/conf.bak > config.txt
-	sed -i -e 's|#{appuser}|'$appuser'|g' -e 's|#{appname}|'$appname'|g' -e 's|#{apppath}|'$apppath'|g' -e 's|#{db_user}|'$db_user'|g' -e 's|#{db_password}|'$db_password'|g' -e 's|#{db_name}|'$db_name'|g' config.txt
+	sed -i.bak -e 's|#{appuser}|'$appuser'|g' -e 's|#{appname}|'$appname'|g' -e 's|#{apppath}|'$apppath'|g' -e 's|#{db_user}|'$db_user'|g' -e 's|#{db_password}|'$db_password'|g' -e 's|#{db_name}|'$db_name'|g' config.txt
+	rm config.txt.bak
 fi
-
+exit 0
